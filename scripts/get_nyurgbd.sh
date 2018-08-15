c38f70a4f0e70e067682ff2fdc3054d0c254c50a
Paul92 <paul.gafton@gmail.com>
2018-08-15 12:57:51 +0100

    Formatting

--- scripts/get_nyurgbd.sh
+++ scripts/get_nyurgbd.sh
@@ -20,59 +20,59 @@ raw_dir="${scene}_raw"
 processed_dir="${scene}_processed"
 
 if [ ! -d "${dataset_dir}/${processed_dir}/${sequence}" ]; then
-    mkdir -p $dataset_dir
-    pushd $dataset_dir
+	mkdir -p $dataset_dir
+	pushd $dataset_dir
 
-    if [ ! -d ${toolbox_dir} ]; then
-	mkdir -p ${toolbox_dir}
-	wget -t2 -d ${toolbox_url}
-	unzip ${toolbox} -d ${toolbox_dir}
-	cp "../../nyu_toolbox/nyu_process.m" ${toolbox_dir}
-	cp "../../nyu_toolbox/fill_depth_colorization.m" ${toolbox_dir}
-	rm ${toolbox}
-    fi
-
-    if [ ! -f "${labelled}" ]; then
-	wget -t2 $labelled_dataset_url
-    fi
-
-    part_no=1
-
-    mkdir -p ${raw_dir}
-    pushd ${raw_dir}
-
-    while : ; do
-	dir="${scene}s_part${part_no}"
-	file="${scene}s_part${part_no}.zip"
-	url="${base_dataset_url}/${file}"
-	(( part_no+=1 ))
-
-	if [ ! -d ${dir} ] && [ ! -f ${file} ]; then
-	    wget -t2 $url
-	    returncode=$?
-	    [[ $returncode -eq 0 ]] || break
-	    mkdir ${dir}
-	    unzip ${file} -d ${dir}
-	    rm -f ${file}
+	if [ ! -d ${toolbox_dir} ]; then
+		mkdir -p ${toolbox_dir}
+		wget -t2 -d ${toolbox_url}
+		unzip ${toolbox} -d ${toolbox_dir}
+		cp "../../nyu_toolbox/nyu_process.m" ${toolbox_dir}
+		cp "../../nyu_toolbox/fill_depth_colorization.m" ${toolbox_dir}
+		rm ${toolbox}
 	fi
-    done
 
-    popd
+	if [ ! -f "${labelled}" ]; then
+		wget -t2 $labelled_dataset_url
+	fi
 
-    finalScenePath=$( find . -name ${sequence} -exec dirname {} \; | cut -c 3- )
+	part_no=1
 
-    processedDir="${scene}_processed"
-    mkdir -p ${processedDir}/${sequence}
+	mkdir -p ${raw_dir}
+	pushd ${raw_dir}
 
-    popd
-    pushd "${dataset_dir}/nyu_toolbox"
+	while : ; do
+		dir="${scene}s_part${part_no}"
+		file="${scene}s_part${part_no}.zip"
+		url="${base_dataset_url}/${file}"
+		(( part_no+=1 ))
 
-    matlab -nodisplay -nodesktop -r "compile;nyu_process('../${finalScenePath}','${sequence}','../${labelled}','../${processedDir}');exit;"
+		if [ ! -d ${dir} ] && [ ! -f ${file} ]; then
+			wget -t2 $url
+			returncode=$?
+			[[ $returncode -eq 0 ]] || break
+			mkdir ${dir}
+			unzip ${file} -d ${dir}
+			rm -f ${file}
+		fi
+	done
 
-    popd
+	popd
+
+	finalScenePath=$( find . -name ${sequence} -exec dirname {} \; | cut -c 3- )
+
+	processedDir="${scene}_processed"
+	mkdir -p ${processedDir}/${sequence}
+
+	popd
+	pushd "${dataset_dir}/nyu_toolbox"
+
+	matlab -nodisplay -nodesktop -r "compile;nyu_process('../${finalScenePath}','${sequence}','../${labelled}','../${processedDir}');exit;"
+
+	popd
 
 else
-    echo "Sequence ${sequence} is already processed; skipping to dataset generation"
+	echo "Sequence ${sequence} is already processed; skipping to dataset generation"
 fi
 
 ./build/bin/dataset-generator -d nyurgbd -i  ${dataset_dir}/${processed_dir}/${sequence} -o $1
